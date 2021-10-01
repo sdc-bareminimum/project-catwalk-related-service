bcbfab6fc276ba29f42799e604580c0e783096cc
Spencer Lepine <spencerlepine26@gmail.com>
2021-09-30 22:13:53 -0700

    af

--- app/test-utils/suiteSetup.js
+++ app/test-utils/suiteSetup.js
@@ -4,6 +4,7 @@ const server = require('../index');
 // Close the entire Sequalize Connection after ALL tests
 // https://stackoverflow.com/questions/60217417/jest-tests-hang-due-to-open-sequelize-connections/60267873#60267873
 afterAll(() => {
+  console.log('closing the server');
   sequelize.close();
   server.close();
 });
