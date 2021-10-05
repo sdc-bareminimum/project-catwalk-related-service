c33e190ba3c9f183a4b9e252eeb24a35bdf4029a
Spencer Lepine <spencerlepine26@gmail.com>
2021-10-05 09:50:06 -0700

    fix connection test

--- app/database/connection.test.js
+++ app/database/connection.test.js
@@ -1,11 +1,18 @@
+require('dotenv').config();
 const { sequelize } = require('./index');
 
 describe('Model Connection', () => {
   it('should connect', (done) => {
     sequelize
       .query('SELECT NOW()')
-      .then((res) => {}) // console.log(res))
-      .catch((err) => console.error(err.stack))
+      .then((res) => {})
+      .catch((err) => err)
+      .then((middleware) => {
+        if (middleware) {
+          console.log(`Connection String: ${process.env.DATABASE_URL}`);
+        }
+        return middleware;
+      })
       .then(done);
   });
 });
