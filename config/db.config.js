71882b73baa08a88a64485428156d2e22850ae07
Spencer Lepine <spencerlepine26@gmail.com>
2021-10-04 19:41:20 -0700

    try fixing docker config

--- config/db.config.js
+++ config/db.config.js
@@ -7,7 +7,7 @@ dotenv.config();
 const connectionString = `postgres://\
 ${process.env.POSTGRES_USER}:\
 ${process.env.POSTGRES_PASSWORD}@\
-${process.env.DATABASE_HOST || '127.0.0.1'}:\
+${process.env.DATABASE_HOST || '0.0.0.0'}:\
 ${process.env.DATABASE_PORT}/\
 ${process.env.POSTGRES_DB}`;
 
@@ -17,7 +17,7 @@ module.exports = {
   database: process.env.POSTGRES_DB,
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
-  host: process.env.DATABASE_HOST, // '127.0.0.1',
+  host: process.env.DATABASE_HOST || '0.0.0.0', // '127.0.0.1',
   dialect: 'postgres',
   // max: 10, // Pool max size
   idleTimeoutMillis: 1000, // Close idle clients after 1 second
