b04ca2caf38ab199604fb71be42308e3acdf787c
Spencer Lepine <spencerlepine26@gmail.com>
2021-10-06 15:05:59 -0700

    rename app folder to src

--- server/src/index.js
+++ server/src/index.js
@@ -10,7 +10,12 @@ const app = express();
 // Parse request body
 app.use(bodyParser.json());
 
-app.use(logger('dev')); // DELETE THIS
+// app.use(logger('dev')); // DELETE THIS
+// Delete this - Loader io verification
+app.use('/loaderio-2759664a2c5b3623b35c0f6d503c5073', (req, res) => {
+  const loaderVerif = 'loaderio-2759664a2c5b3623b35c0f6d503c5073';
+  return res.status(200).json(loaderVerif);
+});
 
 app.use('/', routes);
 
