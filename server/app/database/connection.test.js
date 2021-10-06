61fc677b1849ad765de9c23a7d06ba54915675e8
Spencer Lepine <spencerlepine26@gmail.com>
2021-10-05 20:14:18 -0700

    create working postgres container

--- server/app/database/connection.test.js
+++ server/app/database/connection.test.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
+const { Pool } = require('pg');
 const { sequelize } = require('./index');
 
-describe('Model Connection', () => {
-  it('should connect', (done) => {
+describe('Database Connection', () => {
+  console.log(`Connection String: ${process.env.DATABASE_URL}`);
+
+  it('sequelize should execute a query', (done) => {
     sequelize
       .query('SELECT NOW()')
       .then((res) => {})
@@ -11,8 +14,36 @@ describe('Model Connection', () => {
         if (middleware) {
           console.log(`Connection String: ${process.env.DATABASE_URL}`);
         }
+        expect(middleware).not.toBeDefined();
         return middleware;
       })
       .then(done);
   });
+
+  // it('a pg Pool should connect', (done) => {
+  //   const connectionString = process.env.DATABASE_URL;
+  //   expect(connectionString).toBeDefined();
+
+  //   const pool = new Pool({
+  //     connectionString: connectionString
+  //   });
+
+  //   pool.connect()
+  //     .then(client => {
+  //       return client.query('SELECT CURRENT_DATE')
+  //         .then(res => {
+  //           client.release()
+  //           expect(res.rows[0]).toBeDefined();
+  //         })
+  //         .catch(err => {
+  //           client.release()
+  //           console.log(err.stack) // your callback here
+  //         })
+  //         .then(done)
+  //     })
+  //     .catch(e => {
+  //       expect(e).not.toBeDefined();
+  //     })
+  //     .then(done)
+  // });
 });
